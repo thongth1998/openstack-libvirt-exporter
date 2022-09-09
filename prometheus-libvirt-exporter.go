a4c50345bd35c152ebd2e99518fca414e67603ab
thongth1998 <109638794+thongth1998@users.noreply.github.com>
2022-09-09 15:48:28 +0700

    Update prometheus-libvirt-exporter.go

--- prometheus-libvirt-exporter.go
+++ prometheus-libvirt-exporter.go
@@ -11,7 +11,7 @@ import (
 	"github.com/digitalocean/go-libvirt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/zhangjianweibj/prometheus-libvirt-exporter/libvirt_schema"
+	"github.com/thongth1998/openstack-libvirt-exporter/libvirt_schema"
 	"go.uber.org/zap"
 )
 
